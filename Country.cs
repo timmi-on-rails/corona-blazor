b5bcb1b5813d3629ec716dc3a1b6f0421157a5c8
Tom Scheler <dd-tom@web.de>
2020-05-02 08:47:36 +0200

    added some stats

---
+++ Country.cs
@@ -0,0 +1,11 @@
+﻿namespace CoronaBlazor
+{
+    public class Country
+    {
+        public string Name { get; set; }
+
+        public string Iso2 { get; set; }
+
+        public string Iso3 { get; set; }
+    }
+}
---
+++ DataTotal.cs
@@ -0,0 +1,13 @@
+﻿namespace CoronaBlazor
+{
+    public class DataTotal
+    {
+        public int Confirmed { get; set; }
+
+        public int Recovered { get; set; }
+
+        public int Deaths { get; set; }
+
+        public int Active { get; set; }
+    }
+}
